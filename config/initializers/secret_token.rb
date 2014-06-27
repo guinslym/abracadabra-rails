5a7b2398bbf59fc5e81aeb6fd65706251e4ff388
Guinsly Mondésir <guinslym@gmail.com>
2014-06-27 18:41:51 -0400

    initial commit with gems and User scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AbraTry::Application.config.secret_key_base = '1b511481fbc5584e88023fd7495a18953e123b47bd3171ef99b1ebbb0a543712ecdf8a0bec4df8b040394c7e65d3f6fbc2d07cc3c643f02f8d2c3c6d9cd7dceb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AbraTry::Application.config.session_store :cookie_store, key: '_abra_try_session'
